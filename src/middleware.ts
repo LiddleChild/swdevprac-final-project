42406a8857187a196fa0dafaa6677442cf670f2c
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-11-18 21:04:34 +0700

    fix: middleware problem on vercel

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,10 @@ const adminPaths = ["/booking/manage"];
 export default withAuth(function middleware(req) {}, {
   callbacks: {
     authorized: ({ token, req }) => {
-      if (adminPaths.includes(req.nextUrl.pathname) && token?.role !== "admin") {
-        return false;
+      console.log(token);
+
+      if (adminPaths.includes(req.nextUrl.pathname)) {
+        return token?.role === "admin";
       }
 
       return !!token;
